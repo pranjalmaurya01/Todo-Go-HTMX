4302c01a8f26412bf2e4fed1be4df7d87e8555d8
Pranjal <pranjalmaurya01@gmail.com>
2024-01-22 19:45:11 +0530

    Added complete and incomplete func

--- server.go
+++ server.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,21 +16,61 @@ type todo struct {
 }
 
 func main() {
-	allTodo := []todo{}
+	allTodo := []*todo{}
+	setTodo := map[string]bool{}
 
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
+
 	r.Static("/assets", "./assets")
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
 	})
 
-	r.POST("/add-todo", func(ctx *gin.Context) {
-		tmp := todo{123, "asd", false}
-		allTodo = append(allTodo, tmp)
+	r.POST("/add-todo", func(c *gin.Context) {
+		title := c.PostForm("todo")
 
-		fmt.Println(allTodo)
+		if !setTodo[title] {
+			setTodo[title] = true
+			newId := time.Now().Nanosecond()
+			tmp := &todo{newId, title, false}
+			allTodo = append(allTodo, tmp)
+
+			c.HTML(http.StatusOK, "newTodo.tmpl", gin.H{
+				"todo":    tmp.todo,
+				"id":      tmp.id,
+				"checked": tmp.isComplete,
+			})
+		}
+	})
+
+	r.GET("/toggle-comp-state/:id", func(c *gin.Context) {
+		idPar := c.Param("id")
+		id, err := strconv.Atoi(idPar)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		for i := 0; i < len(allTodo); i++ {
+			v := *allTodo[i]
+			if v.id == id {
+				v.isComplete = !v.isComplete
+
+				allTodo[i] = &v
+
+				fmt.Println(allTodo[i])
+
+				c.HTML(http.StatusOK, "newTodo.tmpl", gin.H{
+					"todo":    v.todo,
+					"id":      v.id,
+					"checked": v.isComplete,
+				})
+				return
+			}
+		}
+
 	})
 
 	r.Run(":3000")
